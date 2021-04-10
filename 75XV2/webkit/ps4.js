f27db9f565b692fe9f0f5a0e5862c756ceacb1f4
Buzbee <31218656+Buzbee@users.noreply.github.com>
2021-04-10 10:22:49 -0400

    Add files via upload

--- 75XV2/webkit/ps4.js
+++ 75XV2/webkit/ps4.js
@@ -199,11 +199,7 @@ function toggle_payload(pld){
 		preloadScripts(['payloads/preloader.js', 'payloads/ps4debug.js', 'payloads/loader.js']);
 	}else if(pld == "goldhen"){
 		document.getElementById("progress").innerHTML="Loading Payload.. Please wait..";
-		if(fw=="755"){
-			preloadScripts(['payloads/preloader.js', 'payloads/goldhen'+fw+'.js', 'payloads/loader.js']);
-		}else{
-			preloadScripts(['payloads/preloader'+fw+'.js', 'payloads/goldhen'+fw+'.js', 'payloads/loader.js']);	
-		}
+		preloadScripts(['payloads/preloader.js', 'payloads/goldhen.js', 'payloads/loader.js']);
 	}else if(pld == "webrte"){
 		document.getElementById("progress").innerHTML="Loading Payload.. Please wait..";
 		preloadScripts(['payloads/preloader.js', 'payloads/webrte.js', 'payloads/loader.js']);
